eb7fbbce1065d0d0e5ec30ac887b495d4ee2082a
Battleroid <casweed@gmail.com>
2015-01-30 19:40:19 -0500

    added 404 handler, message flash, changes to templates

--- contacts.py
+++ contacts.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_whooshee import Whooshee
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
+app.config['WHOOSHEE_DIR'] = 'whooshee'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
 app.config['PAGE_MAX'] = 1
+app.config['SECRET_KEY'] = os.urandom(32)
+db = SQLAlchemy(app)
+whoosh = Whooshee(app)
 
+@whoosh.register_model('fname', 'lname', 'email')
 class Contact(db.Model):
     __tablename__ = 'contacts'
 
@@ -19,14 +25,20 @@ class Contact(db.Model):
     def __repr__(self):
         return '<Contact %r %r %r>' % (self.fname, self.lname, self.email)
 
+@app.errorhandler(404)
+def nope(e):
+    flash("Woops! What you're looking for doesn't exist!")
+    return redirect(url_for('index'))
+
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/page', methods=['GET', 'POST'])
-@app.route('/page/<int:page>', methods=['GET', 'POST'])
+@app.route('/page/<int:page>', methods=['GET'])
 def index(page=1):
-    if request.method == 'POST':
-        query = request.form['search']
-    contact = Contact.query.with_entities(Contact.lname, Contact.fname, Contact.phone, Contact.addr, Contact.email).paginate(page, app.config['PAGE_MAX'], False)
+    # if request.method == 'POST':
+    #     query = request.form['search']
+    #     result = Contact.query.whooshee_search(query).all()
+    #     print query, 'result:', result
+    contact = Contact.query.with_entities(Contact.lname, Contact.fname, Contact.phone, Contact.addr, Contact.email).paginate(page, app.config['PAGE_MAX'], True)
     return render_template('index.html', entry=contact)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
