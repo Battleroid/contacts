2775cfd870104912ff675f6d159c7708d4871a52
Battleroid <casweed@gmail.com>
2015-02-01 02:57:57 -0500

    full text search not possible, whatever, it works

--- contacts.py
+++ contacts.py
@@ -1,26 +1,22 @@
 import os
-from flask import Flask, render_template, request, url_for, redirect, flash
+from flask import Flask, render_template, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_whooshee import Whooshee
 
 app = Flask(__name__)
-app.config['WHOOSHEE_DIR'] = 'whooshee'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
 app.config['PAGE_MAX'] = 1
 app.config['SECRET_KEY'] = os.urandom(32)
 db = SQLAlchemy(app)
-whoosh = Whooshee(app)
 
-@whoosh.register_model('fname', 'lname', 'email')
 class Contact(db.Model):
     __tablename__ = 'contacts'
 
     id = db.Column(db.Integer, primary_key=True)
-    fname = db.Column(db.String, index=True)
-    lname = db.Column(db.String, index=True)
+    fname = db.Column(db.String)
+    lname = db.Column(db.String)
     phone = db.Column(db.String)
     addr = db.Column(db.String)
-    email = db.Column(db.String, unique=True, index=True)
+    email = db.Column(db.String)
 
     def __repr__(self):
         return '<Contact %r %r %r>' % (self.fname, self.lname, self.email)
@@ -33,10 +29,6 @@ def nope(e):
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/page/<int:page>', methods=['GET'])
 def index(page=1):
-    # if request.method == 'POST':
-    #     query = request.form['search']
-    #     result = Contact.query.whooshee_search(query).all()
-    #     print query, 'result:', result
     contact = Contact.query.with_entities(Contact.lname, Contact.fname, Contact.phone, Contact.addr, Contact.email).paginate(page, app.config['PAGE_MAX'], True)
     return render_template('index.html', entry=contact)
 
